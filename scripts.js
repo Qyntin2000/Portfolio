a15b53ab4ed2a3aa33e7172202fbe41898b78c84
Qyntin2000 <87451007+Qyntin2000@users.noreply.github.com>
2024-08-03 05:52:29 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,10 @@ function toggleDropdown(id) {
     const content = document.getElementById(id);
     content.style.display = content.style.display === 'block' ? 'none' : 'block';
 }
+
+document.querySelector('.scroll-icon').addEventListener('click', function() {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+    });
+});
