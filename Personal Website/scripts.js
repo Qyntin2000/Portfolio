99795501f4eb8a670315211c6f61a67738d7a5f4
Qyntin2000 <87451007+Qyntin2000@users.noreply.github.com>
2024-11-06 00:19:39 -0500

    Delete Personal Website directory

--- Personal Website/scripts.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll functionality for the welcome section arrow
-document.querySelector('.scroll-icon').addEventListener('click', function() {
-    window.scrollTo({
-        top: window.innerHeight,
-        behavior: 'smooth'
-    });
-});
