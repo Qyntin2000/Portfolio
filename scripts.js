fcce0360260886ce0045d54ff67bc8eb859fc335
Qyntin2000 <87451007+Qyntin2000@users.noreply.github.com>
2024-11-06 00:01:10 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// scroll functionality for the welcome section arrow
-document.querySelector('.scroll-icon').addEventListener('click', function() {
-    window.scrollTo({
-        top: window.innerHeight,
-        behavior: 'smooth'
-    });
-});
